6bbf6dee3986e03f3bb10f574c184e54446cbdfb
phytalityy <phytalityy@gmail.com>
2026-07-19 23:46:19 -0600

    Delete plugins directory

--- plugins/audiofix/index.js
+++
@@ -1,4 +0,0 @@
-import { ReactNative as RN } from "@vendetta/metro/common";
-import { instead } from "@vendetta/patcher";
-
-export const onUnload = instead("setCommunicationModeOn", RNReactNative.TurboModuleRegistry.get("NativeAudioManagerModule") === null ? RN.TurboModuleRegistry.get("RTNAudioManager") : RN.TurboModuleRegistry.get("NativeAudioManagerModule"), () => {});
